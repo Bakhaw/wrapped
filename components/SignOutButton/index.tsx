18c51a365ebe65a1b8e06376ef701d8c32d2fc26
Hattou Faïssal <faissal.hattou@gmail.com>
2024-03-20 15:43:21 +0100

    :recycle: add redirect after sign out

--- components/SignOutButton/index.tsx
+++ components/SignOutButton/index.tsx
@@ -5,7 +5,11 @@ import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 
 function SignOutButton() {
-  return <Button onClick={() => signOut()}>Sign out</Button>;
+  return (
+    <Button onClick={() => signOut({ callbackUrl: "/sign-in" })}>
+      Sign out
+    </Button>
+  );
 }
 
 export default SignOutButton;
