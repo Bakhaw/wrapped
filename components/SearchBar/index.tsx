9a8a8a317c229a207c2bb6912473b47fb1ae0e80
Hattou Faïssal <faissal.hattou@gmail.com>
2024-04-03 02:08:24 +0200

    :lipstick: search bar

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -38,10 +38,10 @@ function SearchBar({ placeholder = "Search" }: SearchBarProps) {
     <form onSubmit={onSubmit}>
       <div className="flex items-center w-full rounded-full relative">
         <div className="flex items-center relative">
-          <IoSearchOutline className="absolute ml-4 w-5 h-5 text-accent-foreground" />
+          <IoSearchOutline className="absolute ml-4 w-5 h-5 text-accent" />
         </div>
         <input
-          className="w-full py-4 pl-12 pr-4 rounded-full text-accent-foreground"
+          className="w-full py-4 pl-12 pr-4 rounded-full bg-background border border-accent text-accent"
           placeholder={placeholder}
           defaultValue={defaultValue}
           ref={inputRef}
