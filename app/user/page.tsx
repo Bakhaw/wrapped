b546b633ed32f85db9dddce766ca6f740040db75
Hattou Faïssal <faissal.hattou@gmail.com>
2024-04-08 22:57:43 +0200

    :bug: searchParams

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,12 +1,9 @@
-import SearchBar from "@/components/SearchBar";
 import Title from "@/components/Title";
 
 function User() {
   return (
     <section className="flex flex-col gap-8 px-2 py-4">
       <Title>Explore the best wrapppers !</Title>
-
-      <SearchBar />
     </section>
   );
 }
